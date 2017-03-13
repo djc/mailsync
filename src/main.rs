ea8004c37da238d3b81c6d5365bcc9d81dc10a30
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-03-13 21:21:42 +0100

    Initial commit can connect and login

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+extern crate tokio_imap;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::str;
+
+#[derive(Deserialize)]
+struct Config {
+    server: String,
+    account: String,
+    password: String,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut f = File::open(&args[1]).unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+    let config: Config = toml::from_str(&s).unwrap();
+    tokio_imap::run(&config.server, &config.account, &config.password);
+}
