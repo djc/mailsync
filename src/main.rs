6ab44a18ad9b176e39d616d2b83f272d61ea27c8
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-06-05 13:51:48 +0200

    Select Inbox and print responses

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,16 @@ fn main() {
     let handle = core.handle();
     let res = tokio_imap::Client::connect(&config.server, &handle).and_then(|(client, server_greeting)| {
         println!("server: {:?}", server_greeting);
-        client.login(&config.account, &config.password).and_then(|(_, responses)| {
+        client.login(&config.account, &config.password).and_then(|(client, responses)| {
             for rsp in responses.frames {
                 println!("server: {:?}", rsp);
             }
-            ok(())
+            client.select("Inbox").and_then(|(_, responses)| {
+                for rsp in responses.frames {
+                    println!("server: {:?}", rsp);
+                }
+                ok(())
+            })
         })
     });
     core.run(res).unwrap();
