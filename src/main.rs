313797b1f426a722e9ed3993af2f22d19c2a3a27
Dirkjan Ochtman <dirkjan@ochtman.nl>
2017-07-21 11:47:13 +0200

    Add code for loading a message

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,13 @@ extern crate tokio_postgres;
 use futures::future::{Future, ok};
 use futures::stream::{self, Stream};
 use futures_state_stream::StateStream;
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, Read};
 use std::str;
 use tokio_core::reactor::Core;
-use tokio_imap::proto::Attribute;
+use tokio_imap::proto::*;
 use tokio_imap::client::builder::*;
 use tokio_postgres::{Connection, TlsMode};
 
@@ -103,7 +104,171 @@ fn sync_label(ctx: Context, label: &Label) -> Box<ContextFuture> {
 }
 
 fn load_label(ctx: Context, label: &Label) -> Box<ContextFuture> {
-    Box::new(ok(ctx))
+    let Context { client, conn } = ctx;
+    Box::new(
+        client.call(CommandBuilder::select(&label.name))
+            .collect()
+            .map_err(|e| SyncError::from(e))
+            .and_then(|(label_meta, client)| {
+                let exists = label_meta.iter().filter_map(|rd| match *rd.parsed() {
+                    Response::MailboxData(MailboxDatum::Exists(num)) => Some(num),
+                    _ => None,
+                }).nth(0).unwrap();
+                let cmd = CommandBuilder::fetch()
+                    .num(exists)
+                    .attr(Attribute::Uid)
+                    .attr(Attribute::ModSeq)
+                    .attr(Attribute::InternalDate)
+                    .attr(Attribute::Rfc822)
+                    .build();
+                client.call(cmd)
+                    .map_err(|e| SyncError::from(e))
+                    .fold(
+                         ResponseAccumulator::new(4),
+                         |acc, rd| ok::<ResponseAccumulator, SyncError>(acc.push(rd)),
+                    )
+            })
+            .join(conn.prepare("INSERT INTO messages (unid, mod_seq, raw)\
+                                VALUES ($1, $2, $3)")
+                .map_err(|e| SyncError::from(e)))
+            .and_then(|((acc, client), (stmt, conn))| {
+                let ResponseAccumulator { parts, mut full, .. } = acc;
+                assert_eq!(parts.len(), 0);
+                let metas: Vec<Result<MessageMeta, SyncError>> = full.drain().map(
+                    |(_, v)| Ok(v)
+                ).collect();
+                stream::iter(metas).fold((stmt, conn),
+                    |(stmt, conn), meta| {
+                        let raw = match *meta.raw.parsed() {
+                            Response::Fetch(_, ref attr_vals) => {
+                                let val = attr_vals.iter().find(|val|
+                                    match **val {
+                                        AttributeValue::Rfc822(_) => true,
+                                        _ => false,
+                                    }
+                                );
+                                match val {
+                                    Some(&AttributeValue::Rfc822(raw)) => Some(raw),
+                                    _ => None,
+                                }
+                            },
+                            _ => None,
+                        };
+                        println!("execute {} {}!", meta.uid, meta.mod_seq);
+                        conn.execute(&stmt, &[
+                            &(meta.uid as i64),
+                            &(meta.mod_seq as i64),
+                            &raw.unwrap(),
+                        ])
+                        .join(ok(stmt))
+                        .and_then(|((rows, conn), stmt)| {
+                            println!("{} rows", rows);
+                            ok((stmt, conn))
+                        })
+                    }
+                ).join(ok(client))
+            })
+            .and_then(|((_, conn), client)| {
+                client.call(CommandBuilder::close()).collect()
+                    .join(ok(conn))
+                    .map_err(|e| SyncError::from(e))
+            })
+            .and_then(|((_, client), conn)| ok(Context { client, conn }))
+    )
+}
+
+struct ResponseAccumulator {
+    parts: HashMap<u32, (u32, Vec<ResponseData>)>,
+    full: HashMap<u32, MessageMeta>,
+    num_parts: u32,
+}
+
+impl ResponseAccumulator {
+    fn new(num_parts: u32) -> ResponseAccumulator {
+        ResponseAccumulator {
+            parts: HashMap::new(),
+            full: HashMap::new(),
+            num_parts,
+        }
+    }
+    fn push(mut self, rd: ResponseData) -> Self {
+        use AttributeValue::*;
+        let completed = {
+            let (idx, mut entry) = match *rd.parsed() {
+                Response::Fetch(idx, ref attr_vals) => {
+                    let mut entry = self.parts.entry(idx).or_insert((0, vec![]));
+                    for val in attr_vals.iter() {
+                        entry.0 += match *val {
+                            Uid(_) |
+                            ModSeq(_) |
+                            InternalDate(_) |
+                            Rfc822(_) => 1,
+                            _ => 0,
+                        };
+                    }
+                    (idx, entry)
+                },
+                _ => return self,
+            };
+            entry.1.push(rd);
+            if entry.0 == self.num_parts {
+                let mut date = None;
+                let mut mod_seq = None;
+                let mut uid = None;
+                let mut raw = None;
+                for rd in entry.1.drain(..) {
+                    let has_source = match *rd.parsed() {
+                        Response::Fetch(_, ref attr_vals) => {
+                            let mut rsp_source = false;
+                            for val in attr_vals.iter() {
+                                rsp_source |= match *val {
+                                    Uid(u) => {
+                                        uid = Some(u);
+                                        false
+                                    },
+                                    ModSeq(ms) => {
+                                        mod_seq = Some(ms);
+                                        false
+                                    },
+                                    InternalDate(id) => {
+                                        date = Some(id.to_string());
+                                        false
+                                    },
+                                    Rfc822(_) => true,
+                                    _ => false,
+                                }
+                            }
+                            rsp_source
+                        },
+                        _ => false,
+                    };
+                    if has_source {
+                        raw = Some(rd);
+                    }
+                }
+                Some((idx, MessageMeta {
+                    uid: uid.unwrap(),
+                    mod_seq: mod_seq.unwrap(),
+                    date: date.unwrap(),
+                    raw: raw.unwrap(),
+                }))
+            } else {
+                None
+            }
+        };
+        if let Some((idx, meta)) = completed {
+            self.parts.remove(&idx);
+            self.full.insert(idx, meta);
+        }
+        return self;
+    }
+}
+
+struct MessageMeta {
+    uid: u32,
+    mod_seq: u64,
+    date: String,
+    raw: ResponseData,
 }
 
 #[derive(Deserialize)]
