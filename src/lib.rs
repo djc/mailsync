ef4548250bac81009354915a6a8615fa26543465
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-05-03 21:45:13 +0200

    Move date/time parser into library

--- src/lib.rs
+++ src/lib.rs
@@ -107,6 +107,90 @@ impl ResponseAccumulator {
     }
 }
 
+pub fn fuzzy_datetime_parser(orig: &str) -> Option<DateTime<FixedOffset>> {
+    let mut s = orig.to_string();
+    if &s[3..5] == ", " {
+        s = s[5..].trim().to_string();
+    } else if s.starts_with("Wed ") || s.starts_with("Sun ") || s.starts_with("Sat ") {
+        s = s[4..].trim().to_string();
+    } else if s.starts_with(", ") {
+        s = s[2..].trim().to_string();
+    }
+    s = s.replace("GMT+00:00", "+0000");
+    s = s.replace("-0060", "-0100");
+    s = s.replace("-05-30", "-0530");
+    s = s.replace(" t0100", " +0100");
+    let mut parts = s
+        .replace("  ", " ")
+        .split(' ')
+        .take(5)
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+    if parts.len() > 3 && parts[3].contains(".") {
+        let mut t = parts.remove(3);
+        t = t.replace('.', ":");
+        parts.insert(3, t);
+    }
+    if parts.len() > 4 {
+        if parts[4] == "CET" {
+            let _ = parts.remove(4);
+            parts.insert(4, "+0100".to_string());
+        } else if parts[4] == "UT" || parts[4] == "GMT" || parts[4] == "UTC" {
+            let _ = parts.remove(4);
+            parts.insert(4, "+0000".to_string());
+        } else if parts[4] == "CEST" {
+            let _ = parts.remove(4);
+            parts.insert(4, "+0200".to_string());
+        } else if parts[4] == "CST" {
+            let _ = parts.remove(4);
+            parts.insert(4, "-0600".to_string());
+        } else if parts[4] == "PDT" {
+            let _ = parts.remove(4);
+            parts.insert(4, "-0700".to_string());
+        } else if parts[4] == "PST" {
+            let _ = parts.remove(4);
+            parts.insert(4, "-0800".to_string());
+        } else if parts[4] == "EST" {
+            let _ = parts.remove(4);
+            parts.insert(4, "-0500".to_string());
+        } else if parts[4] == "Pacific" && orig.contains("Pacific Standard Time") {
+            let _ = parts.remove(4);
+            parts.insert(4, "-0800".to_string());
+        } else if parts[4] == "%z" && s.contains("(PDT)") {
+            let _ = parts.remove(4);
+            parts.insert(4, "-0700".to_string());
+        } else if parts[3] == "UTC" {
+            let _ = parts.remove(3);
+            parts.insert(3, "+0000".to_string());
+        }
+    }
+    if parts.len() < 5 {
+        parts.push("+0000".to_string());
+    }
+    let new = parts.join(" ");
+    for (i, fmt) in FORMATS.iter().enumerate() {
+        let parsed = DateTime::parse_from_str(&new, fmt);
+        match parsed {
+            Ok(dt) => return Some(dt),
+            Err(_) => {
+                if i == FORMATS.len() - 1 {
+                    //panic!("failed to parse {}, {:?} -> {:?}", e, orig, new);
+                    return None;
+                }
+            }
+        }
+    }
+    unreachable!();
+}
+
+const FORMATS: [&str; 5] = [
+    "%-e %b %Y %H:%M:%S %z",
+    "%-e %b %Y %H:%M %z",
+    "%e %b %Y %H:%M:%S %z",
+    "%e %b %Y %H:%M",
+    "%b %d %H:%M:%S %z %Y",
+];
+
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MessageMeta {
     pub seq: u32,
